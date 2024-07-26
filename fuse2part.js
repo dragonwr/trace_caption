0ef3939fa3fb98f904d9495da3173befad56f936
dragonwr <2783480789@qq.com>
2024-07-26 19:22:44 +0800

    Update fuse2part.js
    use remove for last part

--- fuse2part.js
+++ fuse2part.js
@@ -1,86 +1,91 @@
-                 cpContent.innerHTML = ''
+//                  cpContent.innerHTML = ''
 
-                 // off()
-                 var captionControl2Parent = document.getElementById('ytd-player').parentElement;
+off()
+var captionControl2Parent = document.getElementById('ytd-player').parentElement;
 
-                 var cpElement = captionControl2Parent.querySelector("#cp");
-                 if (!cpElement) {
-                   var cpDiv = document.createElement("div");
-                   cpDiv.id = "cp";
+var cpElement = captionControl2Parent.querySelector("#cp");
+if (!cpElement) {
+  var cpDiv = document.createElement("div");
+  cpDiv.id = "cp";
 
-                   var cpContent = document.createElement("p");
-                   cpContent.id = "cp_content";
-                   cpDiv.appendChild(cpContent);
-                   captionControl2Parent.insertAdjacentElement('afterbegin', cpDiv);
-                 }
-                 // Select the container that holds the caption text lines
-                 captionsContainer = document.querySelector('.captions-text');
+  var cpContent = document.createElement("p");
+  cpContent.id = "cp_content";
+  cpDiv.appendChild(cpContent);
+  captionControl2Parent.insertAdjacentElement('afterbegin', cpDiv);
+}
+// Select the container that holds the caption text lines
+captionsContainer = document.querySelector('.captions-text');
 
-                 // Configuration for the observer (which mutations to observe)
-                 var observerConfig = {
-                   childList: true,
-                   subtree: true,
-                   characterData: true
-                 };
+// Configuration for the observer (which mutations to observe)
+var observerConfig = {
+  childList: true,
+  subtree: true,
+  characterData: true
+};
 
-                 // Callback function to execute when mutations are observed
-                 var mutationCallback = function(mutationsList) {
-                   var hr = false;
-                   for (var i = 0; i < mutationsList.length; i++) {
-                     var mutation = mutationsList[i];
-                     if (mutation.type === 'childList' || mutation.type === 'characterData') {
-                       // Get all child elements within the .captions-text container
-                       captionLines = captionsContainer.children;
+// Callback function to execute when mutations are observed
+var mutationCallback = function(mutationsList) {
+  var hr = false;
+  for (var i = 0; i < mutationsList.length; i++) {
+    var mutation = mutationsList[i];
+    if (mutation.type === 'childList' || mutation.type === 'characterData') {
+      // Get all child elements within the .captions-text container
+      captionLines = captionsContainer.children;
 
-                       if (!hr) {
-                         hr = true
-                         // Get the last child element
-                         lastCaptionLine = captionLines[captionLines.length - 1];
+      if (!hr) {
+        hr = true
+        // Get the last child element
+        lastCaptionLine = captionLines[captionLines.length - 1];
 
-                         // Get the text content of the last caption line
-                         var latestText = lastCaptionLine.innerText || lastCaptionLine.textContent;
-                         var firstPart = []
-                         for (var j = 0; j < latestText.split(' ').length; j++) {
-                           firstPart.push(`<span class='pickle'>${latestText.split(' ')[j]}</span>`)
-                         }
-                         firstPart = firstPart.join(' ')
-                         var ctext = document.querySelector('#en_caption')?.textContent.split(' ') || 'Element not found';
+        // Get the text content of the last caption line
+        var latestText = lastCaptionLine.innerText || lastCaptionLine.textContent;
+        var firstPart = []
+        for (var j = 0; j < latestText.split(' ').length; j++) {
+          firstPart.push(`<span class='pickle'>${latestText.split(' ')[j]}</span>`)
+        }
+        firstPart = firstPart.join(' ')
+        var ctext = document.querySelector('#en_caption')?.textContent.split(' ') || 'Element not found';
 
-                         var wrappedText = [];
-                         for (var j = latestText.split(' ').length; j < ctext.length; j++) {
-                           wrappedText.push(`<span class="onion">${ctext[j]}</span>`);
-                         }
-                         wrappedText = wrappedText.join(' ');
-                         // Log the inner text of the last child
+        var wrappedText = [];
+        remaining = ctext.length
+        for (var j = latestText.split(' ').length; j < remaining; j++) {
+          wrappedText.push(`<span class="onion">${ctext[j]}</span>`);
+        }
+        remaining--;
+        wrappedText = wrappedText.join(' ');
+        // Log the inner text of the last child
 
-                         //   console.log(firstPart, `  ${wrappedText}  `);
-                         let lp = document.createElement('span');
-                         lp.innerHTML = wrappedText
+        //   console.log(firstPart, `  ${wrappedText}  `);
+        let lp = document.createElement('span');
+        lp.innerHTML = wrappedText
 
-                         let fp = document.createElement('span')
-                         fp.innerHTML = latestText.split(' ')[latestText.split(' ').length - 1] + ' '
-                         cpContent.appendChild(fp)
-                         isRoll = document.querySelector('#caption-window-1').classList.contains('ytp-rollup-mode')
-                         if (isRoll) {
-                           cpContent.innerHTML = ''
+        let fp = document.createElement('span')
+        fp.classList.add('pickle')
+        fp.innerHTML = latestText.split(' ')[latestText.split(' ').length - 1] + ' '
+        cpContent.appendChild(fp)
+        document.querySelectorAll('.onion').forEach(element => element.remove());
+        cpContent.appendChild(lp)
+        isRoll = document.querySelector('#caption-window-1').classList.contains('ytp-rollup-mode')
+        if (isRoll) {
+          cpContent.innerHTML = ''
 
-                         }
-                         //          cpContent.innerHTML = firstPart + wrappedText
+        }
+        //          cpContent.innerHTML = firstPart + wrappedText
 
-                         //         end
-                       }
-                     }
-                   }
-                 };
+        //         end
+      }
+    }
+  }
+};
 
-                 // Create an instance of MutationObserver
-                 var observer = new MutationObserver(mutationCallback);
+// Create an instance of MutationObserver
+var observer = new MutationObserver(mutationCallback);
 
-                 // Start observing the container node for configured mutations
-                 observer.observe(captionsContainer, observerConfig);
+// Start observing the container node for configured mutations
+observer.observe(captionsContainer, observerConfig);
 
-                 // Function to stop the observer (optional)
-                 function off() {
-                   observer.disconnect();
-                   console.log('MutationObserver has been stopped.');
-                 }
+// Function to stop the observer (optional)
+function off() {
+  observer.disconnect();
+  console.log('MutationObserver has been stopped.');
+}
